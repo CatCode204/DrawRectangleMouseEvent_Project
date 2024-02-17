1e5a0f03afcd4f0f7be89f7f6348b2259670f97f
Mèo Coder <126305570+CatCode204@users.noreply.github.com>
2024-02-17 15:38:51 +0700

    Update main.cpp
    Make Conflict

--- src/main.cpp
+++ src/main.cpp
@@ -70,6 +70,7 @@ void myDisplay() {
 }
 
 int main(int argc,char* argv[]) {
+    cout << "GLOBAL" << endl;
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition(0,0);
